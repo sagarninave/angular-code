03fc25b395c9c91562e9863ee6ddd5e346221f00
Sagar Ninave <sagar.ninave@neewee.ai>
2022-04-23 23:56:20 +0530

    form controls log

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -9,20 +9,34 @@ import { FormGroup, FormControl } from "@angular/forms";
 export class SignupComponent implements OnInit {
   signup: FormGroup;
 
-  createForm():void{
+  createForm(): void {
     this.signup = new FormGroup({
-      first_name:new FormControl(),
-      last_name:new FormControl(),
-      email:new FormControl(),
-      mobile:new FormControl(),
-      password:new FormControl(),
-    })
+      first_name: new FormControl(),
+      last_name: new FormControl(),
+      email: new FormControl(),
+      mobile: new FormControl(),
+      password: new FormControl(),
+    });
   }
 
-  onSubmit():void{
-    console.log("form Value : ", this.signup.value);
+  onSubmit(): void {
+    console.group("Form Control Property")
+    console.log("Form Group : ", this.signup);
+    console.log("Form Value : ", this.signup.value);
+    console.log("Form Touched : ", this.signup.touched);
+    console.log("Form Controls : ", this.signup.controls);
+    console.groupEnd();
+
+    console.group("Form Control Elements ")
+    console.log("Form Control first_name : ", this.signup.controls.first_name);
+    console.log("Form Control first_name value 1 : ", this.signup.controls.first_name.value);
+    console.log("Form Control first_name value 2 : ", this.signup.get("first_name").value);
+    console.log("Form Control first_name touched : ", this.signup.controls.first_name.touched);
+    console.log("Form Control first_name dirty : ", this.signup.controls.first_name.dirty);
+    console.log("Form Control first_name valid : ", this.signup.controls.first_name.valid);
+    console.groupEnd();
   }
-  
+
   constructor() {}
 
   ngOnInit() {
