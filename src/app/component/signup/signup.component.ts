c0c1b4fac31b046017f84593e447b6162929c8d3
Sagar Ninave <sagar.ninave@neewee.ai>
2022-04-24 20:50:49 +0530

    more form control added

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl } from "@angular/forms";
-
+import { country } from "src/app/constants/country";
 @Component({
   selector: "app-signup",
   templateUrl: "./signup.component.html",
@@ -8,6 +8,7 @@ import { FormGroup, FormControl } from "@angular/forms";
 })
 export class SignupComponent implements OnInit {
   signup: FormGroup;
+  countries: any;
 
   createForm(): void {
     this.signup = new FormGroup({
@@ -16,24 +17,42 @@ export class SignupComponent implements OnInit {
       email: new FormControl(),
       mobile: new FormControl(),
       password: new FormControl(),
+      date_of_birth: new FormControl(),
+      gender: new FormControl(),
+      nationality: new FormControl(),
     });
   }
 
   onSubmit(): void {
-    console.group("Form Control Property")
+    console.group("Form Control Property");
     console.log("Form Group : ", this.signup);
     console.log("Form Value : ", this.signup.value);
     console.log("Form Touched : ", this.signup.touched);
     console.log("Form Controls : ", this.signup.controls);
     console.groupEnd();
 
-    console.group("Form Control Elements ")
+    console.group("Form Control Elements ");
     console.log("Form Control first_name : ", this.signup.controls.first_name);
-    console.log("Form Control first_name value 1 : ", this.signup.controls.first_name.value);
-    console.log("Form Control first_name value 2 : ", this.signup.get("first_name").value);
-    console.log("Form Control first_name touched : ", this.signup.controls.first_name.touched);
-    console.log("Form Control first_name dirty : ", this.signup.controls.first_name.dirty);
-    console.log("Form Control first_name valid : ", this.signup.controls.first_name.valid);
+    console.log(
+      "Form Control first_name value 1 : ",
+      this.signup.controls.first_name.value
+    );
+    console.log(
+      "Form Control first_name value 2 : ",
+      this.signup.get("first_name").value
+    );
+    console.log(
+      "Form Control first_name touched : ",
+      this.signup.controls.first_name.touched
+    );
+    console.log(
+      "Form Control first_name dirty : ",
+      this.signup.controls.first_name.dirty
+    );
+    console.log(
+      "Form Control first_name valid : ",
+      this.signup.controls.first_name.valid
+    );
     console.groupEnd();
   }
 
@@ -41,5 +60,6 @@ export class SignupComponent implements OnInit {
 
   ngOnInit() {
     this.createForm();
+    this.countries = country;
   }
 }
