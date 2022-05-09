44d31537535d5962d4d21be126c763cf98415f95
Sagar Ninave <sagar.ninave@neewee.ai>
2022-05-09 16:47:43 +0530

    signup form created

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SignupComponent } from "./signup/signup.component";
 import { Routes, RouterModule } from "@angular/router";
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from "./login/login.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 const routes: Routes = [
   {
@@ -22,6 +23,6 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/modules/auth/signup/signup.component.ts
+++ src/app/modules/auth/signup/signup.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"],
 })
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor() { }
+  constructor(private FB: FormBuilder) {}
 
   ngOnInit() {
+    this.createForm();
   }
 
+  createForm(): void {
+    this.signupForm = this.FB.group({
+      first_name: [
+        "",
+        [Validators.required, Validators.maxLength, Validators.minLength],
+      ],
+      last_name: [
+        "",
+        [Validators.required, Validators.maxLength, Validators.minLength],
+      ],
+      email: [
+        "",
+        [Validators.required, Validators.maxLength, Validators.minLength],
+      ],
+      password: [
+        "",
+        [Validators.required, Validators.maxLength, Validators.minLength],
+      ],
+    });
+  }
+
+  onSubmitSignup(values): void {
+    console.log("sdsd", values);
+  }
 }
