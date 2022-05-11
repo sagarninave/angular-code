0e33b38a88fbbcc35b4e64b79a79c38abc5dfd49
Sagar Ninave <sagar.ninave@neewee.ai>
2022-05-11 19:37:45 +0530

    signup validation

--- src/app/modules/auth/signup/signup.component.ts
+++ src/app/modules/auth/signup/signup.component.ts
@@ -11,7 +11,7 @@ import { ToastrService } from "ngx-toastr";
 })
 export class SignupComponent implements OnInit {
   signupForm: FormGroup;
-
+  isSubmitted: boolean = false;
   constructor(
     private FB: FormBuilder,
     private auth: AuthService,
@@ -22,38 +22,65 @@ export class SignupComponent implements OnInit {
     this.createForm();
   }
 
+  get f() {
+    return this.signupForm.controls;
+  }
+
   createForm(): void {
     this.signupForm = this.FB.group({
       first_name: [
         "",
-        [Validators.required, Validators.maxLength, Validators.minLength],
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
       ],
       last_name: [
         "",
-        [Validators.required, Validators.maxLength, Validators.minLength],
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
       ],
       email: [
         "",
-        [Validators.required, Validators.maxLength, Validators.minLength],
+        [
+          Validators.required,
+          Validators.email,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"),
+        ],
       ],
       password: [
         "",
-        [Validators.required, Validators.maxLength, Validators.minLength],
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(18),
+        ],
       ],
     });
   }
 
-  onSubmitSignup(values): void {
+  onSubmitSignup(values) {
+    this.isSubmitted = true;
+    if (this.signupForm.status === "INVALID") {
+      return false;
+    }
     values.id = v4();
     values.mobile = "";
     values.date_of_birth = "";
     values.gender = "";
     values.address = "";
-
-    this.auth
-      .onSignup(values)
-      .subscribe((result) =>{
-        this.toastr.success('User registered successfully');
-      } );
+    this.auth.onSignup(values).subscribe((result) => {
+      if (result) {
+        this.isSubmitted = false;
+        this.signupForm.reset();
+        this.toastr.success("User registered successfully");
+      }
+    });
   }
 }
