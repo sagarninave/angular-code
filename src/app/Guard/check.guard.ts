2dc56e4bc69d12e7e564301decd28466d4f9caa9
Sagar Ninave <sagar.ninave@neewee.ai>
2022-05-13 12:54:08 +0530

    removed console

--- src/app/Guard/check.guard.ts
+++ src/app/Guard/check.guard.ts
@@ -10,7 +10,6 @@ export class CheckGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) { }
 
   canActivate() {
-    console.log("this.auth.isLoggedIn() : ", this.auth.isLoggedIn())
     if (this.auth.isLoggedIn()) {
       this.router.navigate(["/user"]);
       return false;
